a33a82f08a0eafae0834c5e4df29a2cb15adc629
Yu Wang <wangyuhere@gmail.com>
2014-07-09 22:10:42 +0200

    Add parse to phrases to indexer

--- lib/feed/indexer.rb
+++ lib/feed/indexer.rb
@@ -1,5 +1,6 @@
 module Feed
   class Indexer
+
     attr_reader :text, :tokens
 
     def initialize(text)
@@ -8,6 +9,20 @@ module Feed
 
     def tokens
       @tokens ||= text.split(/[^\p{L}]+/).reject{|w| w.length < 2}.map(&:downcase)
+    end
+
+    def phrases(gram=2)
+      sub_sentences.map do |s|
+        words = s.split(/[^\p{L}]+/).map(&:downcase)
+        next if words.length < gram
+        (0..(words.length - gram)).map { |i| gram.times.map { |n| words[n+i] }.join(' ') }
+      end.compact.flatten
+    end
+
+    private
+
+    def sub_sentences
+      text.split /[.,;:?!]+\s+/
     end
   end
 end
