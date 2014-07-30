fceaaa78104c82e8e993e1216b422f393d0be6c2
Yu Wang <wangyuhere@gmail.com>
2014-07-30 19:35:08 +0200

    Add services to auto load paths

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,9 @@ module Keywords
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.autoload_paths += %W(#{config.root}/app/presenters)
+    config.autoload_paths += %W(
+      #{config.root}/app/presenters
+      #{config.root}/app/services
+    )
   end
 end
